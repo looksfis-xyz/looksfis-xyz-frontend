32aafc6c8c13f7173bca6a24714e1b05e77774f1
Carson Long <ctlong.970@gmail.com>
2016-04-23 02:27:05 +0800

    feat(services): moved services to folder

--- app/js/app.js
+++ app/js/app.js
@@ -6,8 +6,7 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', [
-  'ionic',
-  'starter.services'
+  'ionic'
 ])
 
 .run(function($ionicPlatform) {
