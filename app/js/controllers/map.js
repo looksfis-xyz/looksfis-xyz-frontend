1aafd839db46dad9588c38b3449ac55ef4418faa
teelu <terrence.lun@gmail.com>
2016-04-23 17:26:58 +0800

    google map working

--- app/js/controllers/map.js
+++ app/js/controllers/map.js
@@ -5,9 +5,11 @@ angular.module('starter')
 .controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
   var options = {timeout: 10000, enableHighAccuracy: true};
   $scope.categories = {}
+  var oldZoom = null;
+  var oldCenter = null;
+  $scope.center_coords = null;
 
   $cordovaGeolocation.getCurrentPosition(options).then(function(position){
-
     var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
     var mapOptions = {
@@ -17,7 +19,27 @@ angular.module('starter')
     };
 
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    //Wait until the map is loaded
+    google.maps.event.addListener($scope.map, 'dragend', function(){
+      $scope.center_coords = $scope.map.getCenter().toUrlValue();
+      console.log($scope.center_coords);
+    });
 
+    google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+
+      var marker = new google.maps.Marker({
+          map: $scope.map,
+          position: latLng,
+          icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 6,
+            strokeWeight: 2,
+            fillColor: '#0ac9fb',
+            strokeColor: 'black',
+            fillOpacity: 1
+          }
+      });
+    });
   }, function(error){
     console.log("Could not get location");
   });
